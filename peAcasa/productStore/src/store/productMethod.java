565769973a83bfbff4e456501b7219267cbce3d5
cqm666 <97065000+cqm666@users.noreply.github.com>
2022-01-11 19:59:15 +0200

    Add files via upload

--- peAcasa/productStore/src/store/productMethod.java
+++ peAcasa/productStore/src/store/productMethod.java
@@ -4,13 +4,17 @@ import store.product;
 public class productMethod<pr> {
     product[] pr = new product[5];
 
+    public void insertProduct(){
+        pr[0] = new product("Sapon", 20.4, "On-Line");
+        pr[1] = new product("Praf-Spalat", 50.6, "Ariel");
+        pr[2] = new product("Pasta de dinti", 90.4, "Lacalut");
+        pr[3] = new product("Prosoape de bucatarie", 41.4, "Panda");
+        pr[4] = new product("Periuta dinti", 16.7, "Orbit");
+    }
+
 
     public void showProduct(){
-        pr[0] = new product("Sapon", 20.4F, "On-Line");
-        pr[1] = new product("Praf-Spalat", 50.6F, "Ariel");
-        pr[2] = new product("Pasta de dinti", 90.4F, "Lacalut");
-        pr[3] = new product("Prosoape de bucatarie", 41.4F, "Panda");
-        pr[4] = new product("Periuta dinti", 16.7F, "Orbit");
+
         for (int i = 0; i < 5; i++) {
 
             pr[i].showProduct();
@@ -19,10 +23,11 @@ public class productMethod<pr> {
     }
 
     public void sumPrice(){
-        int sum=0;
+        double sum=0;
         for (int i = 0; i < 5; i++) {
             sum+=pr[i].pret;
         }
+        System.out.println("***********************");
         System.out.println("Total price  :: " + sum);
     }
 }
--- peAcasa/productStore/src/store/productSwitch.java
+++ peAcasa/productStore/src/store/productSwitch.java
@@ -7,16 +7,19 @@ public class productSwitch {
     Scanner sc = new Scanner(System.in);
     short n;
     productMethod prMethod = new productMethod();
-    public void ProductSwitch(){
 
-        System.out.println("1. Product info ");
-        System.out.println("2. Total price ");
-        System.out.println("3. Exit ");
+    public void ProductSwitch(){
+        continueSwitch:
         while(true){
+            System.out.println("*****************************");
+            System.out.println("1. Product info ");
+            System.out.println("2. Total price ");
+            System.out.println("3. Exit ");
             System.out.print("Choose ::  ");
             n=sc.nextShort();
             switch(n){
                 case 1:
+                    prMethod.insertProduct();
                     prMethod.showProduct();
                     break;
                 case 2:
@@ -25,7 +28,11 @@ public class productSwitch {
                 case 3:
                     System.exit(0);
                     break;
-                default: System.out.println("Ati introdus o optiune gresita");
+                default:
+                    System.out.println("*******************************");
+                    System.out.println("Ati introdus o optiune gresita");
+                    System.out.println("*******************************");
+                continue continueSwitch;
             }
         }
 
